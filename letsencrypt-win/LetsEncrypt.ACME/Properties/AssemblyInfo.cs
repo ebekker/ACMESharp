0151d048184c97f197c2be01214cdd7ac2a6f924
Eugene Bekker <ebekker@powervision.com>
2015-08-24 23:34:04 -0400

    Refactoring assembly-level attributes to support shared info and shared version info

--- letsencrypt-win/LetsEncrypt.ACME/Properties/AssemblyInfo.cs
+++ letsencrypt-win/LetsEncrypt.ACME/Properties/AssemblyInfo.cs
@@ -5,14 +5,15 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LetsEncrypt.ACME")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ACME Client for .NET")]
+[assembly: AssemblyDescription("ACME protocol client library for the .NET platform.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LetsEncrypt.ACME")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+/////////////////////////////////////////////////////////////////
+// Additional "shared" assembly-level attributes are defined in
+// SharedAssemblyInfo.cs and SharedAssemblyVersionInfo.cs files
+/////////////////////////////////////////////////////////////////
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +22,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d551234b-0a8d-4dee-8178-a81998df0edb")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]

f71824980c20ff715ef37856664707935a1c77c5
Eugene Bekker <ebekker@powervision.com>
2015-10-03 23:14:16 -0400

    Adding 'Manual' provider implementations for DNS and WebServer challenge handlers

---
+++ letsencrypt-win/LetsEncrypt.ACME/DNS/ManualDnsProvider.cs
@@ -0,0 +1,43 @@
+﻿using LetsEncrypt.ACME.Util;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsEncrypt.ACME.DNS
+{
+    public class ManualDnsProvider : BaseManualProvider, IDnsProvider
+    {
+        public void EditTxtRecord(string dnsName, IEnumerable<string> dnsValues)
+        {
+            WriteRecord("TXT", dnsName, dnsValues.ToArray());
+        }
+
+        public void EditARecord(string dnsName, string dnsValue)
+        {
+            WriteRecord("A", dnsName, dnsValue);
+        }
+
+        public void EditCnameRecord(string dnsName, string dnsValue)
+        {
+            WriteRecord("CNAME", dnsName, dnsValue);
+        }
+
+        private void WriteRecord(string dnsType, string dnsName, params string[] dnsValues)
+        {
+            _writer.WriteLine("Manually Configure DNS Resource Record:");
+            _writer.WriteLine("  *   Type:  [{0}]", dnsType);
+            _writer.WriteLine("  *   Name:  [{0}]", dnsName);
+
+            if (dnsValues == null || dnsValues.Length == 0)
+                _writer.WriteLine("  *  Value:  (N/A)");
+            else
+            {
+                foreach (var v in dnsValues)
+                    _writer.WriteLine("  *  Value:  [{0}]", v);
+            }
+        }
+    }
+}
---
+++ letsencrypt-win/LetsEncrypt.ACME/WebServer/ManualWebServerProvider.cs
@@ -0,0 +1,39 @@
+﻿using LetsEncrypt.ACME.Util;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsEncrypt.ACME.WebServer
+{
+    public class ManualWebServerProvider : BaseManualProvider, IWebServerProvider
+    {
+        public string FilePath
+        { get; set; }
+
+        public void UploadFile(Uri fileUrl, Stream s)
+        {
+            var path = FilePath;
+
+            if (string.IsNullOrEmpty(path))
+                path = Path.GetTempFileName();
+            else
+            {
+                int index = 0;
+                while (File.Exists(path))
+                    path = string.Format("{0}.{1}", FilePath, ++index);
+            }
+
+            using (var fs = new FileStream(path, FileMode.CreateNew))
+            {
+                s.CopyTo(fs);
+            }
+
+            _writer.WriteLine("Manually Upload Web Server File:");
+            _writer.WriteLine("  *           URL:  [{0}]", fileUrl);
+            _writer.WriteLine("  *  File Content:  [{0}]", path);
+        }
+    }
+}

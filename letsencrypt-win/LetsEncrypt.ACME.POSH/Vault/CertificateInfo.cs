758d9634d20641506ba88c4bf346f3b5146bbd65
Eugene Bekker <ebekker@powervision.com>
2015-10-05 07:12:09 -0400

    Adding vault meta-data classes for ACME Identifiers and Certificate

---
+++ letsencrypt-win/LetsEncrypt.ACME.POSH/Vault/CertificateInfo.cs
@@ -0,0 +1,25 @@
+﻿using LetsEncrypt.ACME.POSH.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsEncrypt.ACME.POSH.Vault
+{
+    public class CertificateInfo : IIdentifiable
+    {
+        public Guid Id
+        { get; set; }
+
+        public string Alias
+        { get; set; }
+
+        public string Label
+        { get; set; }
+
+        public string Memo
+        { get; set; }
+
+    }
+}

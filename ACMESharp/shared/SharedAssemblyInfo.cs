6748268c5eeb948000f659512496359de9791bfa
Eugene Bekker <ebekker@powervision.com>
2015-11-23 20:58:34 -0500

    Renaming top-level solution folder * Updating last remaining references including AppVeyor config and GIT intore file

--- ACMESharp/shared/SharedAssemblyInfo.cs
+++ ACMESharp/shared/SharedAssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyCompany("letsencrypt.org")]
+[assembly: AssemblyCompany("github.com/ebekker/ACMESharp")]
 [assembly: AssemblyProduct("ACMESharp")]
 [assembly: AssemblyCopyright("Copyright © 2015 Eugene Bekker")]
 [assembly: AssemblyTrademark("")]

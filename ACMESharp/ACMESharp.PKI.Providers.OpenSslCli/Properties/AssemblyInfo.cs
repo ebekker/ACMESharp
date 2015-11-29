b1d576d3c296960dfae725a7c0052efaf026c730
Eugene Bekker <ebekker@powervision.com>
2015-11-29 13:40:40 -0500

    Correcting references to shared assembly meta data

--- ACMESharp/ACMESharp.PKI.Providers.OpenSslCli/Properties/AssemblyInfo.cs
+++ ACMESharp/ACMESharp.PKI.Providers.OpenSslCli/Properties/AssemblyInfo.cs
@@ -5,14 +5,15 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ACMESharp.PKI.Providers.OpenSslCli")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ACMESharp PKI Providers - OpenSslCli")]
+[assembly: AssemblyDescription("CertificateProvider for ACMESharp using OpenSSL CLI")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Amazon.com")]
-[assembly: AssemblyProduct("ACMESharp.PKI.Providers.OpenSslCli")]
-[assembly: AssemblyCopyright("Copyright © Amazon.com 2015")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+/////////////////////////////////////////////////////////////////
+// Additional "shared" assembly-level attributes are defined in
+// SharedAssemblyInfo.cs and SharedAssemblyVersionInfo.cs files
+/////////////////////////////////////////////////////////////////
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +22,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5b454575-9688-4a54-bc2b-2d18a15e452b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]

5ea5094dbc65ea5c31e536dc4a39798dee223039
Eugene Bekker <ebekker@powervision.com>
2015-11-29 13:32:50 -0500

    Breaking out arch-specific versions of the OpenSSL Lib CP

---
+++ ACMESharp/ACMESharp.PKI.Providers.OpenSslLib32/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ACMESharp PKI Providers - OpenSslLib32")]
+[assembly: AssemblyDescription("CertificateProvider for ACMESharp using OpenSSL WinLib32")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+/////////////////////////////////////////////////////////////////
+// Additional "shared" assembly-level attributes are defined in
+// SharedAssemblyInfo.cs and SharedAssemblyVersionInfo.cs files
+/////////////////////////////////////////////////////////////////
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2ce2e059-f228-4f88-8440-0413c1af2e4c")]

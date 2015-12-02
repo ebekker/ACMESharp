e74f73cd378c2b7ceb2d3f3bbe98f5afb438be75
Eugene Bekker <ebekker@powervision.com>
2015-12-02 15:47:31 -0500

    Small bump to version
    Also cleaned up the way versions info is captured and repeated.

--- ACMESharp/shared/SharedAssemblyVersionInfo.cs
+++ ACMESharp/shared/SharedAssemblyVersionInfo.cs
@@ -10,9 +10,16 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.0.0")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyVersion(ASMINFO.VERSION + ".0")]
+[assembly: AssemblyFileVersion(ASMINFO.VERSION + ".0")]
 
 // This is used for the NuSpec version tag replacement
 // and is combined with nuget-specific rev and release
-[assembly: AssemblyInformationalVersion("0.7.0")]
+[assembly: AssemblyInformationalVersion(ASMINFO.VERSION)]
+
+// ReSharper disable once InconsistentNaming
+internal class ASMINFO
+{
+    // ReSharper disable once InconsistentNaming
+    public const string VERSION = "0.7.1";
+}

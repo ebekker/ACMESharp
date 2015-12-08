492cc5ce4d8369a3e0074ebd43fab5db35c8cb69
Eugene Bekker <ebekker@powervision.com>
2015-12-08 16:25:46 -0500

    Adding unit test project for Vault
    Start of unit tests for Vault and related elements.

---
+++ ACMESharp/ACMESharp.Vault-test/TestLocalDiskVaultProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACMESharp.Vault
+{
+    [TestClass]
+    public class TestLocalDiskVaultProvider
+    {
+        [TestMethod]
+        public void TestProviderNamesCount()
+        {
+            var all = VaultExtManager.GetProviders();
+
+            // Make sure at least the local disk and the default
+            Assert.IsTrue(all.Count() >= 2);
+        }
+
+        [TestMethod]
+        public void TestDefaultProvider()
+        {
+            var p = VaultExtManager.GetProvider();
+        }
+    }
+}

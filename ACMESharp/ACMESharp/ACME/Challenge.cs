77d817485950238fe055caabb116d5e968b349fc
Eugene Bekker <ebekker@powervision.com>
2015-12-08 15:41:52 -0500

    Defining new extension and provider mechanism for challenge handlers
    * Adding challenge details classes specific to each supported challenge
    type
    * Adding challenge handler and associated provider interfaces
    * Adding extension manager for challenge handlers

---
+++ ACMESharp/ACMESharp/ACME/Challenge.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACMESharp.ACME
+{
+    public enum ChallengeTypeKind
+    {
+        DNS = 1,
+        HTTP = 2,
+        TLS_SNI = 3,
+    }
+
+    public abstract class Challenge
+    {
+        public ChallengeTypeKind TypeKind
+        { get; set; }
+
+        public string Type
+        { get; set; }
+    }
+
+    public class DnsChallenge : Challenge
+    {
+        public string Token
+        { get; set; }
+
+        public string RecordName
+        { get; set; }
+
+        public string RecordValue
+        { get; set; }
+    }
+
+    public class HttpChallenge : Challenge
+    {
+        public string Token
+        { get; set; }
+
+        public string FileUrl
+        { get; set; }
+
+        public string FilePath
+        { get; set; }
+
+        public string FileContent
+        { get; set; }
+    }
+
+    public class TlsSniChallenge : Challenge
+    {
+        public string Token
+        { get; set; }
+
+        public int IterationCount
+        { get; set; }
+
+        // TODO:  this is incomplete!!!
+    }
+}
---
+++ ACMESharp/ACMESharp/ACME/ChallengeHandlerExtManager.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ACMESharp.Ext;
+using System.ComponentModel.Composition.Hosting;
+
+namespace ACMESharp.ACME
+{
+    public interface IChallengeHandlerProviderInfo
+    {
+        string Name
+        { get; }
+
+        string Label
+        { get; }
+
+        string Description
+        { get; }
+    }
+
+    [MetadataAttribute]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class ChallengeHandlerProviderAttribute : ExportAttribute
+    {
+        public ChallengeHandlerProviderAttribute(string name)
+            : base(typeof(IChallengeHandlerProvider))
+        {
+            Name = name;
+        }
+
+        public string Name
+        { get; set; }
+
+        public string Label
+        { get; set; }
+
+        public string Description
+        { get; set; }
+    }
+
+    public class ChallengeHandlerExtManager
+    {
+        private static Config _config;
+
+        public IEnumerable<Tuple<string, IChallengeHandlerProviderInfo>> GetProviders()
+        {
+            return null;
+        }
+
+        public static IChallengeHandlerProvider GetProvider(string name,
+            IDictionary<string, object> initParams = null)
+        {
+            AssertInit();
+            return _config[name]?.Value;
+        }
+
+        static void AssertInit()
+        {
+            if (_config == null)
+            {
+                lock (typeof(Config))
+                {
+                    if (_config == null)
+                    {
+                        InitConfig();
+                    }
+                }
+            }
+            if (_config == null)
+                throw new InvalidOperationException("could not initialize provider configuration");
+        }
+
+        static void InitConfig()
+        {
+            _config = ExtCommon.InitExtConfig<Config>();
+        }
+
+        class Config : Dictionary<string, Lazy<IChallengeHandlerProvider,
+                IChallengeHandlerProviderInfo>>, IExtDetail
+        {
+            private IEnumerable<Lazy<IChallengeHandlerProvider,
+                    IChallengeHandlerProviderInfo>> _Providers;
+
+            public CompositionContainer CompositionContainer
+            { get; set; }
+
+            [ImportMany]
+            public IEnumerable<Lazy<IChallengeHandlerProvider,
+                    IChallengeHandlerProviderInfo>> Providers
+            {
+                get
+                {
+                    return _Providers;
+                }
+
+                set
+                {
+                    _Providers = value;
+                    Clear();
+                    foreach (var x in Providers)
+                    {
+                        var m = x.Metadata;
+
+                        // We can register the provider to the suggested name...
+
+                        // ...if the name is not missing...
+                        if (!string.IsNullOrEmpty(m?.Name))
+                            // ...and the name is not already taken
+                            if (!this.ContainsKey(m.Name))
+                                this[m.Name] = x;
+                    }
+                }
+            }
+        }
+    }
+}

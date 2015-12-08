c9f7d3669b84134dc4845c57354ad4bd97bac160
Eugene Bekker <ebekker@powervision.com>
2015-12-08 16:24:45 -0500

    Adding unit tests for AWS DNS challenge handler

---
+++ ACMESharp/ACMESharp.Providers.AWS-test/TestAwsRoute53Provider.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using ACMESharp.ACME;
+using ACMESharp.POSH.Util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACMESharp.Providers.AWS
+{
+    [TestClass]
+    public class TestAwsRoute53Provider
+    {
+        private static config.DnsConfig _dnsConfig;
+
+        [ClassInitialize]
+        public static void Init(TestContext tctx)
+        {
+            using (var fs = new FileStream("config\\dnsConfig.json", FileMode.Open))
+            {
+                _dnsConfig = JsonHelper.Load<config.DnsConfig>(fs);
+            }
+        }
+
+        public static AwsRoute53ChallengeHandlerProvider GetProvider()
+        {
+            return new AwsRoute53ChallengeHandlerProvider();
+        }
+
+        public static AwsRoute53ChallengeHandler GetHandler(Challenge c)
+        {
+            return (AwsRoute53ChallengeHandler)GetProvider().GetHandler(c, _dnsConfig);
+        }
+
+        [TestMethod]
+        public void TestParameterDescriptions()
+        {
+            var p = GetProvider();
+            var dp = p.DescribeParameters();
+
+            Assert.IsNotNull(dp);
+            Assert.IsTrue(dp.Count() > 0);
+        }
+
+        [TestMethod]
+        public void TestSupportedChallenges()
+        {
+            var p = GetProvider();
+
+            Assert.IsTrue(p.IsSupported(new DnsChallenge()));
+            Assert.IsFalse(p.IsSupported(new HttpChallenge()));
+            Assert.IsFalse(p.IsSupported(new TlsSniChallenge()));
+            Assert.IsFalse(p.IsSupported(new FakeChallenge()));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(KeyNotFoundException))]
+        public void TestRequiredParams()
+        {
+            var p = GetProvider();
+            var c = new DnsChallenge();
+            var h = p.GetHandler(c, new Dictionary<string, object>());
+        }
+
+        [TestMethod]
+        public void TestHandlerLifetime()
+        {
+            var p = GetProvider();
+            var c = new DnsChallenge();
+            var h = p.GetHandler(c, _dnsConfig);
+
+            Assert.IsNotNull(h);
+            Assert.IsFalse(h.IsDisposed);
+            h.Dispose();
+            Assert.IsTrue(h.IsDisposed);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestHandlerDisposedAccess()
+        {
+            var p = GetProvider();
+            var c = new DnsChallenge();
+            var h = p.GetHandler(c, _dnsConfig);
+
+            h.Dispose();
+            h.Handle(null);
+        }
+
+        class FakeChallenge : Challenge
+        { }
+    }
+}
---
+++ ACMESharp/ACMESharp.Providers.AWS-test/config/DnsConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACMESharp.Providers.AWS.config
+{
+    public class DnsConfig : Dictionary<string, object>
+    {
+        public string DefaultDomain
+        {
+            get { return (string)this[nameof(DefaultDomain)]; }
+        }
+
+        public string HostedZoneId
+        {
+            get { return (string)this[nameof(HostedZoneId)]; }
+        }
+
+        public string AccessKeyId
+        {
+            get { return (string)this[nameof(AccessKeyId)]; }
+        }
+
+        public string SecretAccessKey
+        {
+            get { return (string)this[nameof(SecretAccessKey)]; }
+        }
+
+        public string Region
+        {
+            get { return (string)this[nameof(Region)]; }
+        }
+    }
+}

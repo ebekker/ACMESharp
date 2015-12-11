cf16f3bdf8dc2387d0baceca0fbc9d56791d64e3
Eugene Bekker <ebekker@powervision.com>
2015-12-11 07:20:40 -0500

    Cleaning up some imports

--- ACMESharp/ACMESharp/ACME/Providers/ManualChallengeHandler.cs
+++ ACMESharp/ACMESharp/ACME/Providers/ManualChallengeHandler.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ACMESharp.ACME.Providers
 {

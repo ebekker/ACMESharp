4d5ff153811664b6c186f3dd7c95e00b8e8d5d1c
Eugene Bekker <ebekker@powervision.com>
2016-01-09 16:32:40 -0500

    Adding IIS-related providers assembly
    * Based on work of letsencrypt-win-simple project and previous
    IisSitePathProvider from the old provider model

---
+++ ACMESharp/ACMESharp.Providers.IIS/IisWebSite.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACMESharp.Providers.IIS
+{
+    public class IisWebSite
+    {
+        public long SiteId
+        { get; set; }
+
+        public string SiteName
+        { get; set; }
+
+        public string BindingProtocol
+        { get; set; }
+
+        public string BindingAddress
+        { get; set; }
+
+        public string BindingPort
+        { get; set; }
+
+        public string BindingHost
+        { get; set; }
+
+        public string SiteRoot
+        { get; set; }
+    }
+}

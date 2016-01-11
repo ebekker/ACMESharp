f27ac580ab03a710d4bc3db9c5ab950175003d45
Eugene Bekker <ebekker@powervision.com>
2016-01-11 08:57:21 -0500

    Adding ability to override detected PS version
    On ps.org build server, even the the PS3 and PS4 build configurations
    are run on PS5 shell (just with PSx compatibility), so the DNS names
    were still conflicting.

--- ACMESharp/ACMESharp.POSH-test/Pester/000-Common.ps1
+++ ACMESharp/ACMESharp.POSH-test/Pester/000-Common.ps1
@@ -1,9 +1,13 @@
 ﻿
 Set-StrictMode -Version Latest
 
+if (-not $env:PESTER_PSVER) {
+    $env:PESTER_PSVER = $PSVersionTable.PSVersion.Major
+}
+
 $TEST_DNS_ID       = "acme-pester.acmetesting.zyborg.io"
-$TEST_MYIP_DNS_ID  = "acme-pester-ps$($PSVersionTable.PSVersion.Major)-myip.acmetesting.zyborg.io"
-$TEST_DNS_CHLNG_ID = "acme-pester-ps$($PSVersionTable.PSVersion.Major)-chlng.acmetesting.zyborg.io"
+$TEST_MYIP_DNS_ID  = "acme-pester-ps$($env:PESTER_PSVER)-myip.acmetesting.zyborg.io"
+$TEST_DNS_CHLNG_ID = "acme-pester-ps$($env:PESTER_PSVER)-chlng.acmetesting.zyborg.io"
 
 
 if (-not (Get-Variable ACME_POSH_PATH -ValueOnly -ErrorAction Ignore)) {

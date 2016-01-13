f7423787da951930cdf40ded1ababe65ba11cfad
Eugene Bekker <ebekker@powervision.com>
2016-01-13 07:04:37 -0500

    Cleaning up both LOCAL and GLOBAL paths
    As per suggestion in #70, we're checking for both LOCAL and GLOBAL
    module installations regardless of whether we run as Admin and do our
    best to remove in either case.

--- ACMESharp/ACMESharp.POSH-test/choco/acmesharp-posh-all/tools/chocolateyuninstall.ps1
+++ ACMESharp/ACMESharp.POSH-test/choco/acmesharp-posh-all/tools/chocolateyuninstall.ps1
@@ -10,17 +10,37 @@ $isAdmin = Test-ProcessAdminRights
 $localInstallPathSave = "$($env:LOCALAPPDATA)\acmesharp-installpath.txt"
 $globalInstallPathSave = "$toolsDir\acmesharp-installpath.txt"
 
-$installPathSave = if ($isAdmin) { $globalInstallPathSave } else { $localInstallPathSave }
-
-if (Test-Path $installPathSave) {
-    $installPath = [System.IO.File]::ReadAllText($installPathSave)
-}
-else {
+if (-not (Test-Path $localInstallPathSave -PathType Leaf) -and
+        -not (Test-Path $globalInstallPathSave -PathType Leaf)) {
+    Write-Warning "Could not find module in either LOCAL or GLOBAL modules path"
     throw "Unable to resolve module installation path"
 }
 
-if (Test-Path $installPath) {
-    Write-Warning "Removing PS Module directory at [$installPath]"
-    rd $installPath -Recurse
-    del $installPathSave
+if (Test-Path $localInstallPathSave) {
+    $installPath = [System.IO.File]::ReadAllText($localInstallPathSave)
+    if (Test-Path $installPath) {
+        Write-Warning "Removing LOCAL PS Module directory at [$installPath]"
+        rd $installPath -Recurse
+        del $localInstallPathSave
+    }
+    else {
+        Write-Warning "PS Module was expected at LOCAL [$installPath] but was not found"
+    }
+}
+
+if (Test-Path $globalInstallPathSave) {
+    $installPath = [System.IO.File]::ReadAllText($globalInstallPathSave)
+    if (Test-Path $installPath) {
+        Write-Warning "Removing GLOBAL PS Module directory at [$installPath]"
+
+        if (-not $isAdmin) {
+            Write-Warning "Attempting to remove from GLOBAL as non-Admin; this may fail..."
+        }
+
+        rd $installPath -Recurse
+        del $globalInstallPathSave
+    }
+    else {
+        Write-Warning "PS Module was expected at GLOBAL [$installPath] but was not found"
+    }
 }

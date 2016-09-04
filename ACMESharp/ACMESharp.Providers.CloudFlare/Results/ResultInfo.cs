5d1ea5fd00cc5884d68fae761e581cd0d9f904c6
Jan-Pieter Zoutewelle <janpieterzoutewelle@hotmail.com>
2016-09-04 14:54:23 +0200

    Add CloudFlare Provider.

---
+++ ACMESharp/ACMESharp.Providers.CloudFlare/Results/ResultInfo.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace ACMESharp.Providers.CloudFlare.Results
+{
+    internal class ResultInfo
+    {
+        public int Page { get; set; }
+        [JsonProperty("per_page")]
+        public int PerPage { get; set; }
+        [JsonProperty("total_pages")]
+        public int TotalPages { get; set; }
+        public int Count { get; set; }
+        [JsonProperty("total_count")]
+        public int TotalCount { get; set; }
+    }
+}
---
+++ ACMESharp/ACMESharp.Providers.CloudFlare/Results/Zone.cs
@@ -0,0 +1,8 @@
+﻿namespace ACMESharp.Providers.CloudFlare.Results
+{
+    internal class Zone
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}

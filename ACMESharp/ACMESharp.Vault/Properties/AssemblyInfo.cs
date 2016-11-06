f163c6dc1573897b3a9f8be81d8d7107a670a808
Eugene Bekker <ebekker@powervision.com>
2016-11-06 00:27:37 -0400

    This time really fixing broken NUGET version string

--- ACMESharp/ACMESharp.Vault/Properties/AssemblyInfo.cs
+++ ACMESharp/ACMESharp.Vault/Properties/AssemblyInfo.cs
@@ -32,5 +32,18 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.0")]
-[assembly: AssemblyFileVersion("0.8.0")]
+[assembly: AssemblyVersion(ASMINFO.VERSION + ".0")]
+[assembly: AssemblyFileVersion(ASMINFO.VERSION + ".0")]
+
+// This is used for the NuSpec version tag replacement
+// and is combined with nuget-specific rev and release
+[assembly: AssemblyInformationalVersion(ASMINFO.VERSION)]
+
+// ReSharper disable once InconsistentNaming
+internal static class ASMINFO
+{
+
+    // DON'T FORGET TO UPDATE APPVEYOR.YML
+    // ReSharper disable once InconsistentNaming
+    public const string VERSION = "0.8.0";
+}

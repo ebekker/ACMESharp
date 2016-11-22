974c59806b93769f161cb41f1617c87e46376551
Eugene Bekker <ebekker@powervision.com>
2016-11-22 07:34:33 -0500

    Cleaning up file org for default Vault Provider components

---
+++ ACMESharp/ACMESharp.Vault/Providers/EntityMeta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACMESharp.Vault.Providers
+{
+    /// <summary>
+    /// Basic wrapper around any entity that we save using this file-based
+    /// provider in order to track common meta data about the entity.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class EntityMeta<T>
+    {
+        public DateTime CreateDate
+        { get; set; }
+
+        public string CreateUser
+        { get; set; }
+
+        public string CreateHost
+        { get; set; }
+
+        public DateTime UpdateDate
+        { get; set; }
+
+        public string UpdateUser
+        { get; set; }
+
+        public T Entity
+        { get; set; }
+    }
+}
---
+++ ACMESharp/ACMESharp.Vault/Providers/FileVaultAsset.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACMESharp.Vault.Providers
+{
+    public class FileVaultAsset : VaultAsset
+    {
+        public FileVaultAsset(string path, string name, VaultAssetType type, bool isSensitive)
+        {
+            Path = path;
+            Name = name;
+            Type = type;
+            IsSensitive = isSensitive;
+        }
+
+        public string Path
+        { get; set; }
+    }
+}

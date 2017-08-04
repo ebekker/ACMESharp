22eb7e22fe663cae5f4897abb3d41a685809a10a
Eugene Bekker <ebekker@powervision.com>
2017-08-04 15:10:29 -0400

    Updating CI scripts to detect RDP blocking based on DNS instead of file

--- ci/av-onfinish.ps1
+++ ci/av-onfinish.ps1
@@ -1,12 +1,10 @@
-$doRdp = $false
-try { $doRdp = ((wget http://acmesharp.zyborg.io/appveyor-rdp.txt).Content -eq 1) }
-catch { }
-if ($doRdp) {
-  Write-Warning "Detected RDP access request"
-  $blockRdp = $true
-  iex ((new-object net.webclient).DownloadString(
-	  'https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
+## Check if we should Enable RDP access
+if ([int]$((Resolve-DnsName blockrdp.ci.acmesharp.bkkr.us -Type TXT).Text)) {
+    Write-Warning "Detected RDP access request"
+    ## As per:  https://www.appveyor.com/docs/how-to/rdp-to-build-worker/
+    $blockRdp = $true
+    iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
 }
 else {
-  Write-Output "No RDP access requested"
+    Write-Output "No RDP access requested"
 }

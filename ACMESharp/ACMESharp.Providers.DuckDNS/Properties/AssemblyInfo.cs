74a775c3381959a331f35b79ab24f6d7c02948f1
Philipp Nanz <philippn@gmail.com>
2019-03-17 20:57:20 +0100

    Initial commit of Duck DNS extension

---
+++ ACMESharp/ACMESharp.Providers.DuckDNS/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ACMESharp Duck DNS Provider")]
+[assembly: AssemblyDescription("Duck DNS Provider extension for ACMESharp")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("github.com/ebekker/ACMESharp")]
+[assembly: AssemblyProduct("ACMESharp.Providers.DuckDNS")]
+[assembly: AssemblyCopyright("Copyright © 2019 Philipp Nanz. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bc6d303a-71fb-4cc7-8d30-3c3b09f9c92a")]
